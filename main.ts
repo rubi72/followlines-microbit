1647821f2a3f73c53d8da09120a25d7c205afdc8
rubi72 <81889577+rubi72@users.noreply.github.com>
2022-05-19 10:44:44 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 90)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 65)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 82)
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 82)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 65)
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+    }
+})
