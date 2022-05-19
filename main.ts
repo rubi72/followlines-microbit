61bcfec4ce63beeb867f8e18b8f292ab780fa996
rubi72 <81889577+rubi72@users.noreply.github.com>
2022-05-19 11:10:58 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 basic.showIcon(IconNames.Ghost)
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 90)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
     } else {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 65)
@@ -13,6 +15,7 @@ basic.forever(function () {
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             maqueen.motorStop(maqueen.Motors.All)
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
         }
     }
 })
